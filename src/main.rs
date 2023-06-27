6519a2bfee0c40c911ac64a419ab5a1415817557
lancenonce <lancendavis47@gmail.com>
2023-06-27 11:32:23 -0400

    mock main with u8 FHE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use tfhe::{ConfigBuilder, generate_keys, set_server_key, FheUint8};
+use tfhe::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let config = ConfigBuilder::all_disabled()
+        .enable_default_uint8()
+        .build();
+
+    let (client_key, server_key) = generate_keys(config);
+
+    set_server_key(server_key);
+
+    let clear_a = 27u8;
+    let clear_b = 128u8;
+
+    let a = FheUint8::encrypt(clear_a, &client_key);
+    let b = FheUint8::encrypt(clear_b, &client_key);
+
+    let result = a + b;
+
+    let decrypted_result: u8 = result.decrypt(&client_key);
+
+    let clear_result = clear_a + clear_b;
+
+    assert_eq!(decrypted_result, clear_result);
 }
