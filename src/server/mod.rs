fcc26f36dbfc130d4f73ba78dee678b215c103c6
lancenonce <lancendavis47@gmail.com>
2023-06-29 12:52:50 -0400

    added error handling to client

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,12 +1,13 @@
 use std::io::Cursor;
-
 use bincode;
-use tfhe::{FheUint8, ServerKey, set_server_key};
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 use tokio::spawn;
+
+#[allow(unused_imports)]
+use tfhe::{FheUint8, ServerKey, set_server_key};
 
 pub async fn start_server(port: u16) {
     let listener = TcpListener::bind(("127.0.0.1", port)).await.unwrap();
@@ -18,42 +19,41 @@ pub async fn start_server(port: u16) {
 }
 
 async fn handle_client(mut socket: TcpStream) {
-    // Read server key and initial value once at the start
     let mut buffer = vec![0; 1024];
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
-    buffer.truncate(bytes_read);  // Truncate buffer to actual data received
+    socket.read_to_end(&mut buffer).await.unwrap();
+    let mut cursor = Cursor::new(buffer);
 
-    let mut serialized_data = Cursor::new(buffer);
-    let server_key: ServerKey = bincode::deserialize_from(&mut serialized_data).unwrap();
-    let current_value: FheUint8 = bincode::deserialize_from(&mut serialized_data).unwrap();
-    let mut result = current_value.clone();
+    match (bincode::deserialize_from::<_, ServerKey>(&mut cursor), bincode::deserialize_from::<_, FheUint8>(&mut cursor)) {
+        (Ok(server_key), Ok(current_value)) => {
+            let mut result = current_value.clone();
+            set_server_key(server_key);
 
-    set_server_key(server_key);
+            println!("server key and initial value ready");
 
-    println!("server key and initial value ready");
+            loop {
+                let mut buffer = vec![0; 1024];
+                socket.read_to_end(&mut buffer).await.unwrap();
+                if buffer.is_empty() {
+                    // Connection was closed
+                    break;
+                }
 
-    loop {
-        // Read increment value
-        let mut buffer = vec![0; 1024];
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
-        if bytes_read == 0 {
-            // Connection was closed
-            break;
+                let mut cursor = Cursor::new(buffer);
+                if let Ok(increment_value) = bincode::deserialize_from::<_, FheUint8>(&mut cursor) {
+                    println!("Increment value received. Crunching numbers...");
+                    // Increment current_value by increment_value
+                    result += increment_value;
+
+                    // Serialize the result and write to the socket
+                    let result_bytes = bincode::serialize(&result).unwrap();
+                    if let Err(e) = socket.write_all(&result_bytes).await {
+                        println!("Failed to write to socket: {}", e);
+                    }
+                }
+            }
         }
-        buffer.truncate(bytes_read);  // Truncate buffer to actual data received
-
-        let mut serialized_data = Cursor::new(buffer);
-        let increment_value: FheUint8 = bincode::deserialize_from(&mut serialized_data).unwrap();
-        println!("Increment value received. Crunching numbers...");
-
-        // Increment current_value by increment_value
-        result += increment_value;
-
-        // Serialize the result and write to the socket
-        let result_bytes = bincode::serialize(&result).unwrap();
-        socket.write_all(&result_bytes).await.unwrap();
+        _ => {
+            println!("Failed to deserialize server key and/or initial value");
+        }
     }
 }
-
-
-
